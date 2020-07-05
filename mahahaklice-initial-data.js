e1f7d49fe2a298775ac3992b9137779443c04d4c
predrags <predrags@nvidia.com>
2020-07-05 11:48:48 +0200

    wip

--- mahahaklice-initial-data.js
+++ mahahaklice-initial-data.js
@@ -6,7 +6,7 @@ const {v4: uuidv4} = require('uuid');
 casual.seed(2222);
 
 const SPECIES = ['Humans', 'Draenei', 'Dryads', 'Dwarves', 'Gnomes', 'Worgen'];
-casual.define('mahahklica', () => {
+casual.define('mahahaklica', () => {
 	return {
 		itemId: 1,
 		iteration: 1,
@@ -21,7 +21,7 @@ casual.define('mahahklica', () => {
 
 const COUNT = casual.integer(from = 95, to = 105);
 const RANGE = _.range(COUNT);
-const mahahklice = _.reduce(RANGE, (r, i) => {
+const mahahaklice = _.reduce(RANGE, (r, i) => {
 	let max = casual.integer(from = 1, to = 15);
 	max = casual.integer(from = 1, to = max);
 	max = casual.integer(from = 1, to = 2 * max);
@@ -29,13 +29,13 @@ const mahahklice = _.reduce(RANGE, (r, i) => {
 	let itemId = uuidv4();
 	let iterations = _.range(1, max + 1);
 	_.each(iterations, (iteration) => {
-		let mahahklica = casual.mahahklica;
-		mahahklica.itemId = itemId;
-		mahahklica.iteration = iteration;
-		mahahklica.isLatest = iteration === max;
-		r.push(mahahklica);
+		let mahahaklica = casual.mahahaklica;
+		mahahaklica.itemId = itemId;
+		mahahaklica.iteration = iteration;
+		mahahaklica.isLatest = iteration === max;
+		r.push(mahahaklica);
 	});
 	return r;
 }, []);
 
-fs.writeFileSync("mahahklice-initial-data.json", JSON.stringify(mahahklice, null, '\t'));
+fs.writeFileSync("mahahaklice-initial-data.json", JSON.stringify(mahahaklice, null, '\t'));
